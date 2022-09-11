40d9f813c97bef5ba98d0e62de4252582a439714
Taha Elkholy <tahaelkholy.dev@gmail.com>
2022-09-11 20:53:24 +0200

    fix home status bar color

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -9,71 +9,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Encuentra tu\nproducto ideal',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontSize: 24),
-                  ),
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 35,
-                        height: 35,
-                        child: CircleAvatar(
-                          backgroundColor: AppColors.warmBlack.withOpacity(.05),
-                          child: SvgPicture.asset(
-                            'assets/icons/charm_search.svg',
-                            width: 17.5,
-                            height: 17.5,
-                            color: AppColors.warmBlack,
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/clarity_filter-grid-circle-line.svg',
-                        width: 35,
-                        height: 35,
-                      ),
-                    ],
-                  ),
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Encuentra tu\nproducto ideal',
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 20),
+        ),
+        actions: [
+          SizedBox(
+            width: 50,
+            height: 50,
+            child: CircleAvatar(
+              backgroundColor: AppColors.warmBlack.withOpacity(.05),
+              child: SvgPicture.asset(
+                'assets/icons/charm_search.svg',
+                width: 25,
+                height: 25,
+                color: AppColors.warmBlack,
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const HomeBanner(),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    GridView.builder(
-                      shrinkWrap: true,
-                      primary: false,
-                      itemCount: homePageProducts.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2, mainAxisExtent: 325, ),
-                      itemBuilder: (context, index) => ProductItem(
-                        product: homePageProducts[index],
-                      ),
-                    ),
-                  ],
-                ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HomeBanner(),
+            const SizedBox(
+              height: 20,
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              primary: false,
+              itemCount: homePageProducts.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 325,
+              ),
+              itemBuilder: (context, index) => ProductItem(
+                product: homePageProducts[index],
               ),
             ),
           ],
